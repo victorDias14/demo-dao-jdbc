dbd242ee2f0bec9f58cabd5fbd15fe1690cc3ec6
victorDias14 <victodm14@gmail.com>
2022-03-10 12:52:11 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,10 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
+import model.dao.SellerDao;
 
 public class Program {
 
@@ -11,6 +13,9 @@ public class Program {
 
 		var obj = new Department(1, "Books");
 		var seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao(); 
+		
 		System.out.println(seller);
 
 	}
---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoImplJDBC;
+
+public class DaoFactory {
+	
+	public static SellerDao createSellerDao() {
+		return new SellerDaoImplJDBC();
+	}
+
+}
