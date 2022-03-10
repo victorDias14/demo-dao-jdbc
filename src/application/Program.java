f4945ae49f9a633974c70cbf3765d837193bb411
victorDias14 <victodm14@gmail.com>
2022-03-10 12:31:03 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		var obj = new Department(1, "Books");
+		System.out.println(obj);
+
+	}
+
+}
